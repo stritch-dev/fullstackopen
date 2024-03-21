18881e4037190fafe170cec51f446b37de75fc93
jd <stritch.dev@gmail.com>
2024-03-21 14:47:24 -0500

    3.1

---
+++ part3/3.1/index.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.listen(3001, () => {
+ console.log("Server running on port 3001");
+});
+
+const people = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+app.get("/api/persons", (request, response) => {
+ response.json(people);
+});
