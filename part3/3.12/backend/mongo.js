bda9ac9d7a187211a2d442d725742424de2921e5
jd <stritch.dev@gmail.com>
2024-03-25 17:24:20 -0500

    3.12

---
+++ part3/3.12/backend/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const uri = 
+`mongodb+srv://admin:${password}@fullstackpart3.iojrtcv.mongodb.net/?retryWrites=true&w=majority&appName=fullstackpart3`;
+
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
